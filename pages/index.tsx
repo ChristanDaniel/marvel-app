5011010cb0dc72390b0c2fa649924829aeb07f00
ChristianDaniel <christian_daniel04@hotmail.com>
2022-01-31 15:52:53 -0300

    create Feature HomeContainer

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,20 @@
 import type { NextPage } from 'next'
+import Head from 'next/head'
+import dynamic from 'next/dynamic'
+
+const HomeContainer = dynamic(() => import('../src/features/HomeContainer'), {
+  ssr: false
+})
 
 const Home: NextPage = () => {
-  return <div>Hello World</div>
+  return (
+    <>
+      <Head>
+        <title>Marvel | Home</title>
+      </Head>
+      <HomeContainer />
+    </>
+  )
 }
 
 export default Home
---
+++ src/features/HomeContainer/index.tsx
@@ -0,0 +1,14 @@
+// import { useEffect } from 'react'
+import { MainContainer } from './styles'
+
+const HomeContainer = (): JSX.Element => {
+  // useEffect(() => {}, [])
+
+  return (
+    <MainContainer>
+      <h1>Hello World</h1>
+    </MainContainer>
+  )
+}
+
+export default HomeContainer
