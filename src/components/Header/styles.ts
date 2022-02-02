9cca9f18a27eb0aab8b7f2900f40b2038cc2124a
ChristianDaniel <christian_daniel04@hotmail.com>
2022-02-01 21:59:37 -0300

    css Header

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,5 +1,26 @@
 import styled from 'styled-components'
 
-const HeaderContainer = styled.header``
+const HeaderContainer = styled.header`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 100%;
+  height: 100px;
+
+  border-bottom: 1px solid #ffff;
+
+  img {
+    margin-left: 70px;
+  }
+
+  input {
+    height: 35px;
+    width: 280px;
+    padding-left: 20px;
+    border-radius: 15px;
+    border: none;
+    margin-right: 90px;
+  }
+`
 
 export { HeaderContainer }
