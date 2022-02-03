1d565850236d0264330aa0477e7a738a96842b40
ChristianDaniel <christian_daniel04@hotmail.com>
2022-02-02 22:47:14 -0300

    create feature CharacterContainer

---
+++ src/features/CharacterContainer/index.tsx
@@ -0,0 +1,10 @@
+import { MainContainer } from './styles'
+
+const HomeContainer = (): JSX.Element => {
+  return (
+    <MainContainer>
+      <h1>hello wolrd</h1>
+    </MainContainer>
+  )
+}
+export default HomeContainer
