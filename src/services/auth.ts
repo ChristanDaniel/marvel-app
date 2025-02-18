aaac1829fa307618361e792a40835841bf9f24d8
Christian Daniel <christian_daniel04@hotmail.com>
2025-02-18 11:58:58 -0400

    Update auth.ts

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -1,8 +1,8 @@
 import md5 from 'md5'
 
 const time = new Date().getTime()
-const publicKey = '7bf90079c56e5374727e1c2d7b4dbd13'
-const privateKey = 'cff49524fa0df37c865ac3bc3e84133697b5fcfc'
+const publicKey = '55d38ed11eb30d8266c188e95347d927'
+const privateKey = '83d797f0b34d132b9cfc8309213f57672ca1e609'
 const hash = md5(time + privateKey + publicKey)
 const limit = 18
 
